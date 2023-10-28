26659e78e31671dfaee7f48032d0796ac3905481
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-10-27 19:10:27 -0700

    Update Which-are-in.js

--- Which-are-in.js
+++ Which-are-in.js
@@ -17,7 +17,7 @@ Example 2:
 // Solution
 
 function inArray(arr1, arr2) {
-  return arr1.filter(funtion(needle) {
+  return [...new Set(array1)].filter(function(needle) {
     return arr2.some(function(haystack) {
       return haystack.indexOf(needle) > -1;
     });
